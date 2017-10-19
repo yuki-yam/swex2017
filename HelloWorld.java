3b816a21a3bc1435fa23487cf87a6ed1b4725b59
nakasai <nakasai.keitaro.nc8@is.naist.jp>
2017-10-19 15:58:48 +0900

    Good Bye

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Good Bye !!");
+	System.out.println("Good Bye !!");
         System.exit(0);
         System.out.println("Hello NAIST !!");
     }
